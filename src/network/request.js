2f24682d45b6150a039be0e1a2f2c9f1e21521d1
laoshama <56189059+laoshama@users.noreply.github.com>
2020-09-28 14:24:02 +0800

    网络模块封装封装，数据请求成功

---
+++ src/network/request.js
@@ -0,0 +1,26 @@
+// 封装网络请求模块
+import axios from 'axios'
+export function request (config) {
+  // 1、创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:8000/api/z8',
+    timeout: 10000
+  })
+  // 2、axios的拦截器
+  instance.interceptors.request.use(config => {
+    // console.log(config)
+
+    return config
+  }, error => {
+    console.log(error)
+  })
+  // 2.2.响应拦截
+  instance.interceptors.response.use(res => {
+    // console.log(res.data)
+    return res
+  }, error => {
+    console.log(error)
+  })
+  // 3、发送真正的网络请求
+  return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,44 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navigationbar/NavBar'
+import { getHomeMultidata } from 'network/home'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      banners: {},
+      recommends: {},
+      dKeywords: {},
+      keywords: {}
+    }
+  },
+  components: {
+    NavBar
+  },
+  created () {
+    // 1、请求多个数据
+    getHomeMultidata().then(res => {
+      // 箭头函数中的this指向函数外部
+      this.banners = res.data.data.banner
+      this.recommends = res.data.data.recommend
+      // this.dKeywords = res.data.data.dKeyword
+      // this.keywords = res.data.data.keywords
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
